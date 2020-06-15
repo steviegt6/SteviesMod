0f376b8a75150f0e9f8848a27d0ae7ccf603df66
Stevie <xxlennygamerxx@gmail.com>
2020-06-15 15:21:30 -0700

    Bone Sword

---
+++ Content/NPCs/SteviesGlobalNPC.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace SteviesMod.Content.NPCs
+{
+    public class SteviesGlobalNPC : GlobalNPC
+    {
+        public override void NPCLoot(NPC npc)
+        {
+            switch (npc.type)
+            {
+                case NPCID.Skeleton:
+                case NPCID.SmallSkeleton:
+                case NPCID.BigSkeleton:
+                case NPCID.HeadacheSkeleton:
+                case NPCID.SmallHeadacheSkeleton:
+                case NPCID.BigHeadacheSkeleton:
+                case NPCID.MisassembledSkeleton:
+                case NPCID.SmallMisassembledSkeleton:
+                case NPCID.BigMisassembledSkeleton:
+                case NPCID.SkeletonTopHat:
+                case NPCID.SkeletonAstonaut:
+                case NPCID.SkeletonAlien:
+                    if (Main.rand.Next(150) == 0)
+                        Item.NewItem(npc.getRect(), ModContent.ItemType<Items.Weapons.Melee.Shortswords.BoneKnife>());
+                    break;
+            }
+            base.NPCLoot(npc);
+        }
+    }
+}
