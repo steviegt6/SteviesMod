17734ee2b6853ec74807cd6af37f4f78c4963811
Stevie <xxlennygamerxx@gmail.com>
2020-06-15 08:50:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SteviesMod
+ tModLoader 0.11.7.5 mod for Terraria 1.3.5.3.
---
+++ SteviesMod.cs
@@ -0,0 +1,13 @@
+using IL.Terraria;
+using MonoMod.Cil;
+using System;
+using System.Diagnostics;
+using Terraria.ModLoader;
+using Terraria;
+
+namespace SteviesMod
+{
+	public class SteviesMod : Mod
+	{
+    }
+}
