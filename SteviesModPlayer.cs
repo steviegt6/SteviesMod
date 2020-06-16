6b2a7fa8958b5a57a4ab7630831c284a480b2863
Stevie <xxlennygamerxx@gmail.com>
2020-06-15 21:17:13 -0700

    more fishing stuff

--- SteviesModPlayer.cs
+++ SteviesModPlayer.cs
@@ -11,6 +11,13 @@ namespace SteviesMod
 {
     public class SteviesModPlayer : ModPlayer
     {
+        public bool ZonePurity()
+        {
+            if (!player.ZoneBeach && !player.ZoneCorrupt && !player.ZoneCrimson && !player.ZoneDesert && !player.ZoneDungeon && !player.ZoneGlowshroom && !player.ZoneHoly && !player.ZoneJungle && !player.ZoneOldOneArmy && !player.ZoneSnow && !player.ZoneUndergroundDesert)
+                return true;
+            else
+                return false;
+        }
         public override void CatchFish(Item fishingRod, Item bait, int power, int liquidType, int poolSize, int worldLayer, int questFish, ref int caughtType, ref bool junk)
         {
             if (junk)
@@ -24,6 +31,25 @@ namespace SteviesMod
                             break;
                         case 1:
                             caughtType = ItemID.SandstorminaBottle;
+                            break;
+                        default:
+                            break;
+                    }
+            if (ZonePurity())
+                if (Main.rand.NextBool(10))
+                    switch (Main.rand.Next(4))
+                    {
+                        case 0:
+                            caughtType = ItemID.Spear;
+                            break;
+                        case 1:
+                            caughtType = ItemID.Blowpipe;
+                            break;
+                        case 2:
+                            caughtType = ItemID.WoodenBoomerang;
+                            break;
+                        case 3:
+                            caughtType = ItemID.WandofSparking;
                             break;
                         default:
                             break;
