b264ba8112342189abc7c2e8564ac6c1a9953bf8
Stevie <xxlennygamerxx@gmail.com>
2020-06-21 18:40:42 -0700

    phew this took a long time!

--- Content/NPCs/SteviesGlobalNPC.cs
+++ Content/NPCs/SteviesGlobalNPC.cs
@@ -43,7 +43,8 @@ namespace SteviesMod.Content.NPCs
                     }
                     break;
             }
-            CombatText.NewText(npc.Hitbox, Color.White, chat);
+            if (SteviesModConfig.Instance.npcText)
+                CombatText.NewText(npc.Hitbox, Color.White, chat);
             base.GetChat(npc, ref chat);
         }
         public override void OnHitByItem(NPC npc, Player player, Item item, int damage, float knockback, bool crit)
---
+++ SteviesModConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader.Config;
+
+namespace SteviesMod
+{
+    [Label("Stevie's Mod Config")]
+    public class SteviesModConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        public static SteviesModConfig Instance;
+
+        [Header("General Options")]
+        [Label("NPCs DIsplay Text when Chatting")]
+        [DefaultValue(true)]
+        public bool npcText;
+
+        [Label("Fossil UI")]
+        [DefaultValue(true)]
+        public bool fossilUI;
+    }
+}
